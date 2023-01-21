d8ab41b72b3a6f1ecc8b5f0c24057b9f6bc62b3a
ponkotuy <web@ponkotuy.com>
2023-01-22 05:10:19 +0900

    Fix fetch failed on server side rendering

--- view/src/hooks.server.ts
+++ view/src/hooks.server.ts
@@ -4,6 +4,7 @@ import type { HandleFetch } from '@sveltejs/kit';
 export const handleFetch = (({ request, fetch }) => {
 	if (!dev) {
 		const url = new URL(request.url);
+		url.protocol = 'http';
 		url.port = '8080';
 		url.hostname = 'web';
 		request = new Request(url.href, request);
